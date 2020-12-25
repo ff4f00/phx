67da687a7f20067da71b01e818ff790b275003d1
enso <ceo@enso.xyz>
2020-12-25 01:55:47 -0600

    +

--- assets/rollup.config.js
+++ assets/rollup.config.js
@@ -31,7 +31,7 @@ export default {
       // take css output and write it to priv/static
     }),
     
-    css({ output '../priv/static/css/app.css' }),
+    css({ output: '../priv/static/css/app.css' }),
     
     
     resolve({
