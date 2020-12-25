7b06390c3bfe4f58df73d65193f9d47e8d92f241
enso <ceo@enso.xyz>
2020-12-25 01:19:08 -0600

    +

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :phx, PhxWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [yarn: ["watch", cd: Path.expand("../assets", __DIR__)]]
 
 # ## SSL Support
 #
